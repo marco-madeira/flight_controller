c1c3420c4dda1cece48a7ef77d53b4d7f54b80eb
marco-madeira <tuliomarco9704@hotmail.com>
2023-08-14 17:35:16 -0300

    Update requirements.txt

--- flight_control_backend/app/arangoTest.py
+++
@@ -1,6 +0,0 @@
-from api.flights.controller import get_airports_by_distance
-from api.flights.model import Flight
-
-flight = get_airports_by_distance("flights/1000", 500000)    
-for f in flight: 
-  print(f)
